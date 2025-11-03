be74b243c7e499c80e4b7fa858b4442cef1c7be9
Phillip Weinberg <weinbe58@gmail.com>
2025-11-03 16:25:11 -0500

    refactor again

--- src/bloqade/lanes/hypercube.py
+++ src/bloqade/lanes/hypercube.py
@@ -1,4 +1,4 @@
-from itertools import chain
+from itertools import product
 from typing import TypeVar
 
 from .types import ArchSpec, Block, Grid, InterLane, IntraLane
@@ -16,7 +16,7 @@ def intra_hypercube(dim: int) -> tuple[IntraLane, ...]:
     return tuple(intra_lanes)
 
 
-def partition_grid(sites: Grid, num_x: int, num_y: int):
+def partition_grid(sites: Grid, num_x: int, num_y: int, row_major: bool = True):
     if sites.shape[0] % num_x != 0:
         raise ValueError("Number of x sites must be divisible by num_x")
     if sites.shape[1] % num_y != 0:
@@ -25,18 +25,22 @@ def partition_grid(sites: Grid, num_x: int, num_y: int):
     block_size_x = sites.shape[0] // num_x
     block_size_y = sites.shape[1] // num_y
 
-    blocks: list[list[Block[tuple[float, float]]]] = []
-    for by in range(num_y):
-        row_blocks = []
-        for bx in range(num_x):
+    blocks: list[Block[tuple[float, float]]] = []
+
+    if row_major:
+        for by, bx in product(range(num_y), range(num_x)):
             x_start = bx * block_size_x
             x_end = x_start + block_size_x
             y_start = by * block_size_y
             y_end = y_start + block_size_y
-            row_blocks.append(
-                Block(sites=sites[x_start:x_end, y_start:y_end].positions)
-            )
-        blocks.append(row_blocks)
+            blocks.append(Block(sites=sites[x_start:x_end, y_start:y_end].positions))
+    else:
+        for bx, by in product(range(num_x), range(num_y)):
+            x_start = bx * block_size_x
+            x_end = x_start + block_size_x
+            y_start = by * block_size_y
+            y_end = y_start + block_size_y
+            blocks.append(Block(sites=sites[x_start:x_end, y_start:y_end].positions))
 
     return blocks
 
@@ -46,46 +50,40 @@ Ny = TypeVar("Ny")
 
 
 def holobyte_geometry(
-    compute_sites: Grid[Nx, Ny], cache_sites: Grid[Nx, Ny], memory_sites: Grid[Nx, Ny]
+    compute_sites: Grid[Nx, Ny], cache_sites: Grid[Nx, Ny], *memory_sites: Grid[Nx, Ny]
 ):
     num_compute_bytes_x = compute_sites.shape[0] // 16
     num_compute_bytes_y = compute_sites.shape[1]
 
-    num_mem_bytes_x = memory_sites.shape[0] // 16
-    num_mem_bytes_y = memory_sites.shape[1]
-
     if compute_sites.shape != cache_sites.shape:
         raise ValueError("Compute sites and cache sites must have the same shape")
 
-    if num_compute_bytes_x != num_mem_bytes_x:
-        raise ValueError(
-            "Compute sites and memory sites must have the same number of holobytes in x dimension"
-        )
-
-    if num_mem_bytes_y % num_compute_bytes_y != 0:
-        raise ValueError(
-            "Memory sites must have an integer multiple of holobytes in y dimension compared to compute sites"
-        )
+    for mem_sites in memory_sites:
+        # make sure each memory block matches compute block dimensions
+        if mem_sites.shape[0] % 16 != num_compute_bytes_x * 16:
+            raise ValueError(
+                "Memory sites x dimension must be an integer multiple of compute sites x dimension"
+            )
+        if mem_sites.shape[1] != num_compute_bytes_y:
+            raise ValueError(
+                "Memory block sites y dimension must match compute sites y dimension"
+            )
 
     compute_sites = compute_sites[: num_compute_bytes_x * 16, :]
     cache_sites = cache_sites[: num_compute_bytes_x * 16, :]
-    memory_sites = memory_sites[: num_mem_bytes_x * 16, :]
 
     compute_blocks = partition_grid(compute_sites, 16, 1)
     cache_blocks = partition_grid(cache_sites, 16, 1)
-    memory_blocks = partition_grid(memory_sites, 16, 1)
 
-    blocks = chain.from_iterable(compute_blocks + cache_blocks + memory_blocks)
-    has_intra_blocks = frozenset(chain.from_iterable(compute_blocks))
+    blocks = compute_blocks + cache_blocks
+    has_intra_blocks = frozenset(compute_blocks)
 
     inter_lanes = []
-    for mem_y_id, mem_row in enumerate(memory_blocks):
-        cache_y_id = (
-            mem_y_id % num_compute_bytes_y
-        )  # linearly map memory rows to cache rows
-        for mem_x_id, mem_block in enumerate(mem_row):
-            cache_block = cache_blocks[cache_y_id][mem_x_id]
-            for site_id in range(16):
+    for mem_sites in memory_sites:
+        memory_blocks = partition_grid(mem_sites, 16, 1)
+        blocks.extend(memory_blocks)
+        for mem_block, cache_block in zip(memory_blocks, cache_blocks):
+            for site_id in range(len(cache_block.sites)):
                 inter_lane = InterLane(
                     block_1=mem_block,
                     block_2=cache_block,
