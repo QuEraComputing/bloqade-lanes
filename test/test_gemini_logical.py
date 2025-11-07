344cc8ae754b3b19170030b90ab5455db112cc21
Phillip Weinberg <weinbe58@gmail.com>
2025-11-07 11:11:28 -0500

    adding proper test

--- test/test_gemini_logical.py
+++ test/test_gemini_logical.py
@@ -1,29 +1,38 @@
 from matplotlib import pyplot as plt
 
 from bloqade.lanes.gemini import generate_arch
-
-arch_physical = generate_arch()
-
-f, axs = plt.subplots(1, 1)
+from bloqade.lanes.types.encoding import EncodingType
 
 
-ax = arch_physical.plot(
-    show_words=(0, 1), show_intra=tuple(range(4)), show_inter=(0,), ax=axs
-)
+def test_architecture_generation():
+    arch_physical = generate_arch()
 
-ax.set_aspect(0.25)
-xmin, xmax = ax.get_xlim()
-ymin, ymax = ax.get_ylim()
-ax.set_xlim(xmin - 2, xmax + 2)
-ax.set_ylim(ymin - 2, ymax + 2)
+    assert len(arch_physical.words) == 16
+    assert len(arch_physical.site_buses) == 5
+    assert len(arch_physical.word_buses) == 4
+    assert arch_physical.encoding is EncodingType.BIT32
 
 
-f, axs = plt.subplots(2, 2, figsize=(10, 8))
+def plot():
+    arch_physical = generate_arch()
 
-arch_physical.plot(show_words=tuple(range(16)), show_inter=(0,), ax=axs[0, 0])
-arch_physical.plot(show_words=tuple(range(16)), show_inter=(1,), ax=axs[0, 1])
-arch_physical.plot(show_words=tuple(range(16)), show_inter=(2,), ax=axs[1, 0])
-arch_physical.plot(show_words=tuple(range(16)), show_inter=(3,), ax=axs[1, 1])
+    f, axs = plt.subplots(1, 1)
 
+    ax = arch_physical.plot(
+        show_words=(0, 1), show_intra=tuple(range(4)), show_inter=(0,), ax=axs
+    )
 
-plt.show()
+    ax.set_aspect(0.25)
+    xmin, xmax = ax.get_xlim()
+    ymin, ymax = ax.get_ylim()
+    ax.set_xlim(xmin - 2, xmax + 2)
+    ax.set_ylim(ymin - 2, ymax + 2)
+
+    f, axs = plt.subplots(2, 2, figsize=(10, 8))
+
+    arch_physical.plot(show_words=tuple(range(16)), show_inter=(0,), ax=axs[0, 0])
+    arch_physical.plot(show_words=tuple(range(16)), show_inter=(1,), ax=axs[0, 1])
+    arch_physical.plot(show_words=tuple(range(16)), show_inter=(2,), ax=axs[1, 0])
+    arch_physical.plot(show_words=tuple(range(16)), show_inter=(3,), ax=axs[1, 1])
+
+    plt.show()
