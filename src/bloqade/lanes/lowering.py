6914901f3482b48d9b31d12292562aa791f2e9f5
Phillip Weinberg <weinbe58@gmail.com>
2025-11-22 12:58:23 -0500

    Implement Lowering from `bloqade.native` to `bloqade.lanes.move` dialects.  (#33)
    * adding implementation lowering from native to move dialect
    
    * fixing tests

---
+++ src/bloqade/lanes/lowering.py
@@ -0,0 +1,99 @@
+from dataclasses import dataclass
+from typing import Callable
+
+from bloqade.analysis import address
+from bloqade.rewrite.passes import AggressiveUnroll
+from kirin import ir, passes, rewrite
+from kirin.ir.method import Method
+
+from bloqade.lanes.analysis import layout, placement
+from bloqade.lanes.dialects import circuit, move
+from bloqade.lanes.rewrite import circuit2move, native2circuit
+
+
+def default_merge_heuristic(region_a: ir.Region, region_b: ir.Region) -> bool:
+    return False
+
+
+@dataclass
+class NativeToCircuit:
+    merge_heuristic: Callable[[ir.Region, ir.Region], bool] = default_merge_heuristic
+
+    def emit(self, mt: Method, no_raise: bool = True):
+        out = mt.similar(mt.dialects.add(circuit))
+        AggressiveUnroll(out.dialects, no_raise=no_raise).fixpoint(out)
+        rewrite.Walk(
+            native2circuit.RewriteLowLevelCircuit(),
+        ).rewrite(out.code)
+
+        rewrite.Fixpoint(
+            rewrite.Walk(native2circuit.MergePlacementRegions(self.merge_heuristic))
+        ).rewrite(out.code)
+        passes.TypeInfer(out.dialects)(out)
+
+        return out
+
+
+@dataclass
+class CircuitToMove:
+    layout_heristic: layout.LayoutHeuristicABC
+    placement_strategy: placement.PlacementStrategyABC
+    move_scheduler: circuit2move.MoveSchedulerABC
+    insert_palindrome_moves: bool = True
+
+    def emit(self, mt: Method, no_raise: bool = True):
+        out = mt.similar(mt.dialects.add(move))
+
+        if no_raise:
+            address_frame, _ = address.AddressAnalysis(out.dialects).run_no_raise(out)
+            initial_layout = layout.LayoutAnalysis(
+                out.dialects, self.layout_heristic, address_frame.entries
+            ).get_layout_no_raise(out)
+            placement_frame, _ = placement.PlacementAnalysis(
+                out.dialects,
+                initial_layout,
+                address_frame.entries,
+                self.placement_strategy,
+            ).run_no_raise(out)
+        else:
+            address_frame, _ = address.AddressAnalysis(out.dialects).run(out)
+            initial_layout = layout.LayoutAnalysis(
+                out.dialects, self.layout_heristic, address_frame.entries
+            ).get_layout(out)
+            placement_frame, _ = placement.PlacementAnalysis(
+                out.dialects,
+                initial_layout,
+                address_frame.entries,
+                self.placement_strategy,
+            ).run(out)
+
+        placement_analysis = placement_frame.entries
+        args = (self.move_scheduler, placement_analysis)
+        rule = rewrite.Chain(
+            circuit2move.InsertInitialize(initial_layout),
+            circuit2move.InsertMoves(*args),
+            circuit2move.RewriteCZ(*args),
+            circuit2move.RewriteR(*args),
+            circuit2move.RewriteRz(*args),
+        )
+        rewrite.Walk(rule).rewrite(out.code)
+
+        if self.insert_palindrome_moves:
+            rewrite.Walk(circuit2move.InsertPalindromeMoves()).rewrite(out.code)
+
+        rewrite.Walk(
+            rewrite.Chain(
+                circuit2move.LiftMoveStatements(), circuit2move.RemoveNoOpCircuits()
+            )
+        ).rewrite(out.code)
+
+        rewrite.Fixpoint(
+            rewrite.Walk(
+                rewrite.Chain(
+                    circuit2move.DeleteQubitNew(), rewrite.DeadCodeElimination()
+                )
+            )
+        ).rewrite(out.code)
+        passes.TypeInfer(out.dialects)(out)
+
+        return out
