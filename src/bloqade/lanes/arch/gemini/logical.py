0aee4b2a37bc5ca7e19a1fcd19cfce82ab7f41b8
Phillip Weinberg <weinbe58@gmail.com>
2025-11-30 16:30:54 -0500

    refactor assuming lanes are valid

--- src/bloqade/lanes/arch/gemini/logical.py
+++ src/bloqade/lanes/arch/gemini/logical.py
@@ -40,58 +40,25 @@ class RewriteMoves(rewrite_abc.RewriteRule):
     def get_y_positions(self, lane: LaneAddress):
         return lane.site_id // 2
 
-    def get_site_bus_info(
-        self, node: ir.Statement, lanes: tuple[SiteLaneAddress, ...]
-    ) -> tuple[ir.SSAValue | None, int | None, int | None, Direction | None]:
+    def get_address_info(self, node: ir.Statement, lanes: tuple[LaneAddress, ...]):
         direction = lanes[0].direction
         y_positions_list = ilist.IList([self.get_y_positions(lane) for lane in lanes])
         word = lanes[0].word_id
         bus_id = lanes[0].bus_id
 
-        if any(lane.direction is not direction for lane in lanes):
-            direction = None
-        if any(lane.word_id != word for lane in lanes):
-            word = None
-        if any(lane.bus_id != bus_id for lane in lanes):
-            bus_id = None
-
         (y_positions_stmt := py.Constant(y_positions_list)).insert_before(node)
+
         return y_positions_stmt.result, word, bus_id, direction
-
-    def get_word_bus_info(
-        self, node: ir.Statement, lanes: tuple[WordLaneAddress, ...]
-    ) -> tuple[ir.SSAValue | None, Direction | None]:
-        direction = lanes[0].direction
-        y_positions_list = ilist.IList([self.get_y_positions(lane) for lane in lanes])
-        bus_id = lanes[0].bus_id
-        word_id = lanes[0].word_id
-
-        if any(lane.direction is not direction for lane in lanes):
-            direction = None
-        if any(lane.bus_id != bus_id for lane in lanes):
-            bus_id = None
-        if any(lane.word_id != word_id for lane in lanes):
-            word_id = None
-
-        (y_positions_stmt := py.Constant(y_positions_list)).insert_before(node)
-        return y_positions_stmt.result, direction
 
     def rewrite_Statement(self, node: ir.Statement):
         if not isinstance(node, move.Move):
             return rewrite_abc.RewriteResult()
 
-        lanes = node.lanes
+        y_positions, word, bus_id, direction = self.get_address_info(
+            node, lanes := node.lanes
+        )
 
         if types.is_tuple_of(lanes, SiteLaneAddress):
-            y_positions, word, bus_id, direction = self.get_site_bus_info(node, lanes)
-            if (
-                y_positions is None
-                or word is None
-                or bus_id is None
-                or direction is None
-            ):
-                return rewrite_abc.RewriteResult()
-
             node.replace_by(
                 SiteBusMove(
                     y_positions,
@@ -102,10 +69,6 @@ class RewriteMoves(rewrite_abc.RewriteRule):
             )
             return rewrite_abc.RewriteResult(has_done_something=True)
         elif types.is_tuple_of(lanes, WordLaneAddress):
-            y_positions, direction = self.get_word_bus_info(node, lanes)
-            if y_positions is None or direction is None:
-                return rewrite_abc.RewriteResult()
-
             node.replace_by(
                 WordBusMove(
                     y_positions,
