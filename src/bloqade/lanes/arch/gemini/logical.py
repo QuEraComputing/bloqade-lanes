e36adf157e064527dbbf90066f745987648278c6
Phillip Weinberg <weinbe58@gmail.com>
2025-12-01 09:56:29 -0500

    Removing validation from rewrite

--- src/bloqade/lanes/arch/gemini/logical.py
+++ src/bloqade/lanes/arch/gemini/logical.py
@@ -1,14 +1,15 @@
+from dataclasses import dataclass, field
+
 from kirin import decl, ir, types
 from kirin.decl import info
 from kirin.dialects import ilist, py
 from kirin.rewrite import abc as rewrite_abc
 
 from bloqade.lanes.dialects import move
+from bloqade.lanes.layout.arch import ArchSpec
 from bloqade.lanes.layout.encoding import (
     Direction,
-    LaneAddress,
-    SiteLaneAddress,
-    WordLaneAddress,
+    MoveType,
 )
 
 from .impls import generate_arch
@@ -35,43 +36,32 @@ class WordBusMove(ir.Statement):
     direction: Direction = info.attribute()
 
 
+@dataclass
 class RewriteMoves(rewrite_abc.RewriteRule):
+    """Re"""
 
-    def get_y_positions(self, lane: LaneAddress):
-        return lane.site_id // 2
+    arch_spec: ArchSpec = field(default_factory=get_arch_spec)
 
     def get_address_info(self, node: move.Move):
 
-        if len(node.lanes) == 0:
-            return None, None, None, None
+        move_type = node.lanes[0].move_type
         direction = node.lanes[0].direction
         word = node.lanes[0].word_id
         bus_id = node.lanes[0].bus_id
 
-        if not all(lane.word_id == word for lane in node.lanes):
-            word = None
-        if not all(lane.bus_id == bus_id for lane in node.lanes):
-            bus_id = None
-        if not all(lane.direction == direction for lane in node.lanes):
-            direction = None
-
-        y_positions_list = ilist.IList(
-            [self.get_y_positions(lane) for lane in node.lanes]
-        )
+        y_positions_list = ilist.IList([lane.site_id // 2 for lane in node.lanes])
         (y_positions_stmt := py.Constant(y_positions_list)).insert_before(node)
 
-        return y_positions_stmt.result, word, bus_id, direction
+        return move_type, y_positions_stmt.result, word, bus_id, direction
 
     def rewrite_Statement(self, node: ir.Statement):
-        if not isinstance(node, move.Move):
+        if not (isinstance(node, move.Move) and len(node.lanes) > 0):
             return rewrite_abc.RewriteResult()
 
-        y_positions, word, bus_id, direction = self.get_address_info(node)
+        # This assumes validation has already occurred so only valid moves are present
+        move_type, y_positions, word, bus_id, direction = self.get_address_info(node)
 
-        if y_positions is None or word is None or bus_id is None or direction is None:
-            return rewrite_abc.RewriteResult()
-
-        if types.is_tuple_of(node.lanes, SiteLaneAddress):
+        if move_type is MoveType.SITE:
             node.replace_by(
                 SiteBusMove(
                     y_positions,
@@ -80,17 +70,14 @@ class RewriteMoves(rewrite_abc.RewriteRule):
                     direction=direction,
                 )
             )
-            return rewrite_abc.RewriteResult(has_done_something=True)
-        elif types.is_tuple_of(node.lanes, WordLaneAddress):
-            if word != 0 or bus_id != 0:
-                return rewrite_abc.RewriteResult()
-
+        elif move_type is MoveType.WORD:
             node.replace_by(
                 WordBusMove(
                     y_positions,
                     direction=direction,
                 )
             )
-            return rewrite_abc.RewriteResult(has_done_something=True)
         else:
-            return rewrite_abc.RewriteResult()
+            raise AssertionError("Unsupported move type for rewrite")
+
+        return rewrite_abc.RewriteResult(has_done_something=True)
