18cd99f54b5b0500a5c6bc661bb3d1657740d2c3
Phillip Weinberg <weinbe58@gmail.com>
2025-12-07 16:30:47 -0500

    refactor rewrite for simulation

--- src/bloqade/lanes/arch/gemini/logical/simulation/rewrite.py
+++ src/bloqade/lanes/arch/gemini/logical/simulation/rewrite.py
@@ -1,33 +1,32 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, replace
+from typing import TypeVar
 
 from kirin import ir
 from kirin.rewrite import abc as rewrite_abc
 
 from bloqade.lanes.dialects import move
-from bloqade.lanes.layout.encoding import LocationAddress
+from bloqade.lanes.layout.encoding import LaneAddress, LocationAddress
+
+AddressType = TypeVar("AddressType", bound=LocationAddress | LaneAddress)
+
+
+def physical_word_id(address: AddressType) -> tuple[AddressType, ...]:
+    if address.word_id == 0:
+        return tuple(replace(address, word_id=word_id) for word_id in range(7))
+    elif address.word_id == 1:
+        return tuple(replace(address, word_id=word_id) for word_id in range(9, 16, 1))
+    else:
+        return (address,)
 
 
 @dataclass
 class RewriteFill(rewrite_abc.RewriteRule):
-    def get_locations(self, address: LocationAddress):
-        if address.word_id == 0:
-            return tuple(
-                LocationAddress(word_id, address.site_id) for word_id in range(7)
-            )
-        elif address.word_id == 1:
-            return tuple(
-                LocationAddress(word_id, address.site_id) for word_id in range(9, 16, 1)
-            )
-        else:
-            return (address,)
 
     def rewrite_Statement(self, node: ir.Statement):
         if not isinstance(node, move.Fill):
             return rewrite_abc.RewriteResult()
 
-        transversal_addresses = sum(
-            map(self.get_locations, node.location_addresses), ()
-        )
+        transversal_addresses = sum(map(physical_word_id, node.location_addresses), ())
 
         node.replace_by(move.Fill(location_addresses=transversal_addresses))
 
