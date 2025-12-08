f28eba141aecff73289ced054ba6f3588a145ffa
Phillip Weinberg <weinbe58@gmail.com>
2025-12-07 21:19:27 -0500

    adding rewrite rule for moves + refactor

--- src/bloqade/lanes/arch/gemini/logical/simulation/rewrite.py
+++ src/bloqade/lanes/arch/gemini/logical/simulation/rewrite.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass, replace
-from typing import TypeVar
+from itertools import chain
+from typing import Callable, Iterator, TypeVar
 
 from kirin import ir
 from kirin.rewrite import abc as rewrite_abc
@@ -10,24 +11,52 @@ from bloqade.lanes.layout.encoding import LaneAddress, LocationAddress
 AddressType = TypeVar("AddressType", bound=LocationAddress | LaneAddress)
 
 
-def physical_word_id(address: AddressType) -> tuple[AddressType, ...]:
+def physical_word_id(address: AddressType) -> Iterator[AddressType]:
     if address.word_id == 0:
-        return tuple(replace(address, word_id=word_id) for word_id in range(7))
+        yield from (replace(address, word_id=word_id) for word_id in range(7))
     elif address.word_id == 1:
-        return tuple(replace(address, word_id=word_id) for word_id in range(9, 16, 1))
+        yield from (replace(address, word_id=word_id) for word_id in range(9, 16, 1))
     else:
-        return (address,)
+        yield address
 
 
 @dataclass
-class RewriteFill(rewrite_abc.RewriteRule):
+class RewriteLocations(rewrite_abc.RewriteRule):
+
+    transform_location: Callable[[LocationAddress], Iterator[LocationAddress]] = (
+        physical_word_id
+    )
 
     def rewrite_Statement(self, node: ir.Statement):
-        if not isinstance(node, move.Fill):
+        if not isinstance(
+            node, (move.Fill, move.LocalR, move.LocalRz, move.Initialize)
+        ):
             return rewrite_abc.RewriteResult()
 
-        transversal_addresses = sum(map(physical_word_id, node.location_addresses), ())
+        physical_addresses = tuple(
+            chain.from_iterable(map(self.transform_location, node.location_addresses))
+        )
 
-        node.replace_by(move.Fill(location_addresses=transversal_addresses))
+        attributes: dict[str, ir.Attribute] = {
+            "location_addresses": ir.PyAttr(physical_addresses)
+        }
+
+        node.replace_by(node.from_stmt(node, attributes=attributes))
+        return rewrite_abc.RewriteResult(has_done_something=True)
+
+
+@dataclass
+class RewriteMoves(rewrite_abc.RewriteRule):
+    transform_lanes: Callable[[LaneAddress], Iterator[LaneAddress]] = physical_word_id
+
+    def rewrite_Statement(self, node: ir.Statement):
+        if not isinstance(node, move.Move):
+            return rewrite_abc.RewriteResult()
+
+        physical_lanes = tuple(
+            chain.from_iterable(map(self.transform_lanes, node.lanes))
+        )
+
+        node.replace_by(move.Move(lanes=physical_lanes))
 
         return rewrite_abc.RewriteResult(has_done_something=True)
