dfe5febc17e39aea8e30d0a5a021c19a7fdc4fbf
Phillip Weinberg <weinbe58@gmail.com>
2025-12-29 15:05:41 -0500

    going back to old demo

--- demo/msd.py
+++ demo/msd.py
@@ -11,14 +11,13 @@ kernel.run_pass = squin.kernel.run_pass
 
 
 @kernel
-def main(cond: bool):
+def main():
     # see arXiv: 2412.15165v1, Figure 3a
     reg = qubit.qalloc(5)
     squin.broadcast.t(reg)
 
     squin.broadcast.sqrt_x(ilist.IList([reg[0], reg[1], reg[4]]))
-    if cond:
-        squin.broadcast.cz(ilist.IList([reg[0], reg[2]]), ilist.IList([reg[1], reg[3]]))
+    squin.broadcast.cz(ilist.IList([reg[0], reg[2]]), ilist.IList([reg[1], reg[3]]))
     squin.broadcast.sqrt_y(ilist.IList([reg[0], reg[3]]))
     squin.broadcast.cz(ilist.IList([reg[0], reg[3]]), ilist.IList([reg[2], reg[4]]))
     squin.sqrt_x_adj(reg[0])
