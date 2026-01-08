70618bad5249d8967d5627f97294e3381fbfc2dd
Phillip Weinberg <weinbe58@gmail.com>
2026-01-08 16:30:14 -0500

    test for the base.py submodule

--- src/bloqade/lanes/analysis/atom/__init__.py
+++ src/bloqade/lanes/analysis/atom/__init__.py
@@ -2,6 +2,7 @@ from . import impl as impl
 from .analysis import (
     AtomInterpreter as AtomInterpreter,
 )
+from .atom_state_data import AtomStateData as AtomStateData
 from .lattice import (
     AtomState as AtomState,
     Bottom as Bottom,
