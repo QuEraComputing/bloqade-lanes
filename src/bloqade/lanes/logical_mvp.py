439db49cc15427aed110cfc86891e1227ac706c3
Rafael Haenel <rhaenel@quera.com>
2026-01-09 14:35:49 -0500

    Notebook for decoding of observables

--- src/bloqade/lanes/logical_mvp.py
+++ src/bloqade/lanes/logical_mvp.py
@@ -1,5 +1,7 @@
 import io
+from typing import Any
 
+from bloqade.gemini import logical as gemini_logical
 from bloqade.gemini.rewrite.initialize import __RewriteU3ToInitialize
 from bloqade.native.upstream import SquinToNative
 from bloqade.rewrite.passes import CallGraphPass
@@ -7,7 +9,10 @@ from bloqade.squin.rewrite.non_clifford_to_U3 import RewriteNonCliffordToU3
 from bloqade.stim.emit.stim_str import EmitStimMain
 from bloqade.stim.upstream.from_squin import squin_to_stim
 from kirin import ir, passes, rewrite
+from kirin.dialects import ilist
+import numpy as np
 
+from bloqade import annotate, squin, types
 from bloqade.lanes import visualize
 from bloqade.lanes.arch.gemini import logical
 from bloqade.lanes.arch.gemini.impls import generate_arch
@@ -17,6 +22,42 @@ from bloqade.lanes.rewrite import transversal
 from bloqade.lanes.rewrite.move2squin.noise import NoiseModelABC
 from bloqade.lanes.transform import MoveToSquin
 from bloqade.lanes.upstream import NativeToPlace, PlaceToMove
+import stim
+from tsim import Circuit
+from tesseract_decoder import tesseract
+
+# Kernel definition with gemini logical dialect and annotate
+kernel = squin.kernel.add(gemini_logical.dialect).add(annotate)
+kernel.run_pass = squin.kernel.run_pass
+
+
+class Decoder:
+    def __init__(self, dem: stim.DetectorErrorModel):
+        config = tesseract.TesseractConfig(dem=dem)
+        self.num_observables = dem.num_observables
+        self._decoder = config.compile_decoder()
+
+    def decode(self, detector_bits: np.ndarray) -> np.ndarray:
+
+        logical_bits_flips = np.zeros(
+            (detector_bits.shape[0], self.num_observables), dtype=bool
+        )
+        for i, detector_shot in enumerate(detector_bits):
+            flip_logical_bit = self._decoder.decode(detector_shot)
+            logical_bits_flips[i] = flip_logical_bit
+        return logical_bits_flips
+
+
+@kernel
+def set_detector(meas: ilist.IList[types.MeasurementResult, Any]):
+    annotate.set_detector([meas[0], meas[1], meas[2], meas[3]], coordinates=[0, 0])
+    annotate.set_detector([meas[1], meas[2], meas[4], meas[5]], coordinates=[0, 1])
+    annotate.set_detector([meas[2], meas[3], meas[4], meas[6]], coordinates=[0, 2])
+
+
+@kernel
+def set_observable(meas: ilist.IList[types.MeasurementResult, Any], idx: int):
+    annotate.set_observable([meas[0], meas[1], meas[5]], idx)
 
 
 def compile_squin_to_move(mt: ir.Method, transversal_rewrite: bool = False):
@@ -117,7 +158,7 @@ def compile_to_physical_squin_noise_model(
 
 def compile_to_physical_stim_program(
     mt: ir.Method, noise_model: NoiseModelABC | None = None
-) -> str:
+) -> Circuit:
     """Compiles a logical squin kernel to a physical stim kernel with noise channels inserted.
 
     Args:
@@ -127,6 +168,8 @@ def compile_to_physical_stim_program(
         The compiled physical stim program as a string.
 
     """
+    from tsim import Circuit
+
     noise_kernel = compile_to_physical_squin_noise_model(mt, noise_model)
     noise_kernel = squin_to_stim(noise_kernel)
 
@@ -135,4 +178,4 @@ def compile_to_physical_stim_program(
     emit.initialize()
     emit.run(node=noise_kernel)
 
-    return buf.getvalue().strip()
+    return Circuit(buf.getvalue().strip())
