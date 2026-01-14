QuEraComputing/bloqade-lanes

commit be5459d3af9c669b87cc2e8f47d7be995613c66b

Bug fixes and updating demo.  (#100)
* updating annotation

* adding function to do transversal rewrites

* adding new demo as jupytext

* fixing conflict between scf and cf lowering

* making the qubit initialization optional

* updating move_lang.py to explicitly generate squin

* updating move_lang.py demo to demo generating noise model

* fix conflict by discarding lowering for scf

src/bloqade/lanes/rewrite/move2squin/noise.py
@@ -130,7 +130,7 @@ class InsertNoise(AtomStateRewriter):
         move_noise_methods = tuple(map(self.noise_model.get_lane_noise, node.lanes))
         qubit_ssas = self.get_qubit_ssa_from_locations(
             atom_state,
-            tuple(self.arch_spec.get_endpoints(lane)[1] for lane in node.lanes),
+            tuple(self.arch_spec.get_endpoints(lane)[0] for lane in node.lanes),
         )
         qubit_ssas = list(filter(None, qubit_ssas))
 

demo/move_lang.py
@@ -1,111 +1,38 @@
 from bloqade.lanes import kernel
-from bloqade.lanes.analysis import atom
-from bloqade.lanes.analysis.atom.atom_state_data import AtomStateData
-from bloqade.lanes.analysis.atom.lattice import AtomState, IListResult, Unknown
 from bloqade.lanes.arch.gemini.logical import get_arch_spec
 from bloqade.lanes.dialects import move
 from bloqade.lanes.layout.encoding import (
-    Direction,
     LocationAddress,
     SiteLaneAddress,
     WordLaneAddress,
     ZoneAddress,
 )
+from bloqade.lanes.noise_model import generate_simple_noise_model
+from bloqade.lanes.transform import MoveToSquin
+
+lane1 = SiteLaneAddress(0, 0, 0)
+lane2 = WordLaneAddress(0, 5, 0)
 
 
 @kernel
 def main(cond: bool):
-    state0 = move.load()
-    state1 = move.fill(
-        state0, location_addresses=(LocationAddress(0, 0), LocationAddress(1, 0))
+    state = move.load()
+    state = move.fill(
+        state, location_addresses=(LocationAddress(0, 0), LocationAddress(1, 0))
     )
-    state2 = move.move(state1, lanes=(SiteLaneAddress(0, 0, 0),))
-    state3 = move.move(state2, lanes=(WordLaneAddress(0, 5, 0),))
-
-    if cond:
-        state4 = move.move(
-            state3, lanes=(SiteLaneAddress(1, 0, 0, Direction.BACKWARD),)
-        )
-    else:
-        state4 = move.cz(state3, zone_address=ZoneAddress(0))
-
-    state5 = move.move(state4, lanes=(WordLaneAddress(0, 5, 0, Direction.BACKWARD),))
-    state6 = move.move(state5, lanes=(SiteLaneAddress(0, 0, 0, Direction.BACKWARD),))
-    move.store(state6)
-
-    return [state0, state1, state2, state3, state4, state5, state6]
+    state = move.move(state, lanes=(lane1,))
+    state = move.move(state, lanes=(lane2,))
+    state = move.cz(state, zone_address=ZoneAddress(0))
+    state = move.move(state, lanes=(lane2.reverse(),))
+    state = move.move(state, lanes=(lane1.reverse(),))
+    move.store(state)
 
 
-frame, result = atom.AtomInterpreter(kernel, arch_spec=get_arch_spec()).run(main)
-main.print(analysis=frame.entries)
+arch_spec = get_arch_spec()
 
+squin_kernel = MoveToSquin(
+    arch_spec=get_arch_spec(),
+    noise_model=generate_simple_noise_model(),
+).emit(main)
 
-assert result == IListResult(
-    data=(
-        AtomState(
-            data=AtomStateData(
-                locations_to_qubit={},
-                qubit_to_locations={},
-                collision={},
-                prev_lanes={},
-                move_count={},
-            )
-        ),
-        AtomState(
-            data=AtomStateData(
-                locations_to_qubit={
-                    LocationAddress(word_id=0, site_id=0): 0,
-                    LocationAddress(word_id=1, site_id=0): 1,
-                },
-                qubit_to_locations={
-                    0: LocationAddress(word_id=0, site_id=0),
-                    1: LocationAddress(word_id=1, site_id=0),
-                },
-                collision={},
-                prev_lanes={},
-                move_count={},
-            )
-        ),
-        AtomState(
-            data=AtomStateData(
-                locations_to_qubit={
-                    LocationAddress(word_id=1, site_id=0): 1,
-                    LocationAddress(word_id=0, site_id=5): 0,
-                },
-                qubit_to_locations={
-                    0: LocationAddress(word_id=0, site_id=5),
-                    1: LocationAddress(word_id=1, site_id=0),
-                },
-                collision={},
-                prev_lanes={
-                    0: SiteLaneAddress(
-                        word_id=0, site_id=0, bus_id=0, direction=Direction.FORWARD
-                    )
-                },
-                move_count={0: 1},
-            )
-        ),
-        AtomState(
-            data=AtomStateData(
-                locations_to_qubit={
-                    LocationAddress(word_id=1, site_id=0): 1,
-                    LocationAddress(word_id=1, site_id=5): 0,
-                },
-                qubit_to_locations={
-                    0: LocationAddress(word_id=1, site_id=5),
-                    1: LocationAddress(word_id=1, site_id=0),
-                },
-                collision={},
-                prev_lanes={
-                    0: WordLaneAddress(
-                        word_id=0, site_id=5, bus_id=0, direction=Direction.FORWARD
-                    )
-                },
-                move_count={0: 2},
-            )
-        ),
-        Unknown(),
-        Unknown(),
-        Unknown(),
-    )
-)
+squin_kernel.print()

demo/pipeline_details.py
@@ -1,0 +1,221 @@
+# %% [markdown]
+# # Example: Gemini Logical Lanes Compilation Pipeline
+# ---
+# In this example, we demonstrate the compilation pipeline for a simple quantum kernel
+# using the Gemini logical lanes architecture. We will walk through the steps of compiling
+# a basic squin kernel, performing layout and placement analyses, rewriting to move dialect,
+# and finally inverting back to the squin dialect with physical qubits and noise models inserted.
+
+# to start we create a simple squin kernel that uses the gemini logical dialect to define a
+# terminal measurement on the circuit.
+
+from bloqade.gemini import logical as gemini_logical
+
+# %%
+from bloqade import annotate, squin
+
+kernel = squin.kernel.add(gemini_logical.dialect).add(annotate)
+kernel.run_pass = squin.kernel.run_pass
+
+
+@kernel
+def example_kernel():
+    reg = squin.qalloc(2)
+
+    squin.cx(reg[0], reg[1])
+
+    gemini_logical.terminal_measure(reg)
+
+
+example_kernel.print()
+
+# %% [markdown]
+# ## Compilation Pipeline
+# The first step is to compile the logical squin kernel to the native gate set.
+# this can be done by using the existing SquinToNative transformer inside bloqade-circuit.
+
+# %%
+from bloqade.native.upstream import SquinToNative  # noqa: E402
+
+example_kernel = SquinToNative().emit(example_kernel)
+example_kernel.print()
+
+# %% [markdown]
+# Note that the kernel looks identical to before, because the rewrite pass rewrites the call
+# graph (after copying) but does not change the call graph structure itself.
+
+# %% [markdown]
+# Next, we lowering from the native dialect to the place dialect. The place dialect attempts
+# to encapsulate the circuit into code blocks stored in regions which can be used later on for
+# placement and routing analyses. Another important aspect of the insert special qubit allocation
+# statements that are used to indicate the logical state prepration of the qubits. Given the nature
+# of how the current move synthesis works the call graph is flattened out into a single kernel.
+
+# %%
+from bloqade.lanes.upstream import NativeToPlace  # noqa: E402
+
+example_kernel = NativeToPlace().emit(example_kernel)
+example_kernel.print()
+
+# %% [markdown]
+# now that we have the place dialect inside the kernel we can run the initial layout analysis
+# to determine the initial placement of logical qubits onto the gemini logical lanes architecture.
+
+# part of the input to the layout analysis is the layout heuristic which takes data collected from
+# the analysis interpreter to generate the initial layout. In this example we use a heuristic
+# that priorities placing qubits that interact frequently within the same word. This heuristic
+# is implemented in the `fixed` module inside bloqade-lanes.
+
+# %%
+from bloqade.analysis import address  # noqa: E402
+
+from bloqade.lanes.analysis import layout  # noqa: E402
+from bloqade.lanes.heuristics import fixed  # noqa: E402
+
+address_frame, _ = address.AddressAnalysis(example_kernel.dialects).run(example_kernel)
+
+
+layout_analysis = layout.LayoutAnalysis(
+    example_kernel.dialects,
+    fixed.LogicalLayoutHeuristic(),
+    address_frame.entries,
+    tuple(range(2)),
+)
+
+initial_layout = layout_analysis.get_layout(example_kernel)
+print(initial_layout)
+
+# %% [markdown]
+# After running the layout analysis we can use it to plot the initial placement of logical qubits
+
+# %%
+from bloqade.lanes.arch.gemini.logical import get_arch_spec  # noqa: E402
+
+logical_arch = get_arch_spec()
+
+ax = logical_arch.plot(show_words=(0, 1))
+
+(pos_0,) = logical_arch.get_positions(initial_layout[0])
+(pos_1,) = logical_arch.get_positions(initial_layout[1])
+ax.scatter(
+    [pos_0[0], pos_1[0]],
+    [pos_0[1], pos_1[1]],
+    s=200,
+    label="Initial Placement",
+)
+
+# %% [markdown]
+# Next, we can run the placement analysis to determine the intermediate  placement of logical qubits
+# during the circuit execution. The difference here being that the placement analysis will be
+# mapped onto the linear logic within the bodies of the StaticPlacement statements.
+
+# %%
+from bloqade.lanes.analysis import placement  # noqa: E402
+
+placement_analysis = placement.PlacementAnalysis(
+    example_kernel.dialects,
+    initial_layout,
+    address_frame.entries,
+    fixed.LogicalPlacementStrategy(),
+)
+
+placement_frame, _ = placement_analysis.run(example_kernel)
+example_kernel.print(analysis=placement_frame.entries)
+
+# %% [markdown]
+# With the placement analysis complete we can now proceed to rewrite the place dialect to the
+# move dialect. This involves using the MoveScheduler to insert move operations before gates
+# in the place dialect to based on the current placement of qubits and the placement of the qubits
+# required for the gate operation. There are also options to insert palindrome moves after gates
+# to move the qubits back to their original locations at the end of the StaticPlacement body.
+# Here we never have more than one CZ operation within a StaticPlacement body because we want
+# the logical qubits to always return back to their original locations after each gate. Hence
+# we set `insert_palindrome_moves=True` in the MoveToSquin transformer.
+
+
+# %%
+from bloqade.lanes.upstream import PlaceToMove  # noqa: E402
+
+example_kernel = PlaceToMove(
+    layout_heristic=fixed.LogicalLayoutHeuristic(),
+    placement_strategy=fixed.LogicalPlacementStrategy(),
+    move_scheduler=fixed.LogicalMoveScheduler(),
+    insert_palindrome_moves=True,
+).emit(example_kernel)
+
+example_kernel.print()
+
+# %% [markdown]
+# Now at this point we can either continue to hardware by specializing the move dialect
+# to a gemini specific statements which can be further lowered to pulse level control. We
+# will not cover that here, instead we will demonstrate to integrate with the tsim but rewriting
+# the move program back to squin dialect on the physical qubits along with inserting noise.
+
+# Before we rewrite to physical squin we can also run the atom state analysis on the logical program
+# just to demonstrate how the analysis works on the move dialect.
+
+# To start we simply construct the atom interpreter with the logical architecture spec, then
+# run the analysis on the move dialect kernel.
+
+# %%
+from bloqade.lanes.analysis import atom  # noqa: E402
+from bloqade.lanes.arch.gemini.logical import get_arch_spec  # noqa: E402
+
+frame, _ = atom.AtomInterpreter(example_kernel.dialects, arch_spec=logical_arch).run(
+    example_kernel
+)
+example_kernel.print(analysis=frame.entries)
+
+# %% [markdown]
+
+# Now if we want the physical noise model of the program we first have to rewrite from logical moves
+# on the logical architecture to physical squin on the physical architecture. Because all the gates
+# are clifford gates and can be implemented transversally we simply rewrite all logical addresses
+# to groups of physical addresses. If you are interested in how this is done please see the
+# `transversal_rewrites` function inside the `bloqade.lanes.logical_mvp` module.
+
+# %%
+from bloqade.lanes.logical_mvp import transversal_rewrites  # noqa: E402
+
+example_kernel = transversal_rewrites(example_kernel)
+example_kernel.print()
+
+# %% [markdown]
+# Now that we have the physical move kernel we can run the atom state analysis again on the physical
+# architecture to get the atom states on physical qubits.
+
+# %%
+from bloqade.lanes.arch.gemini.impls import generate_arch  # noqa: E402
+
+physical_arch = generate_arch(4)
+
+frame, _ = atom.AtomInterpreter(example_kernel.dialects, arch_spec=physical_arch).run(
+    example_kernel
+)
+example_kernel.print(analysis=frame.entries)
+
+# %% [markdown]
+# from here we can rewrite the physical move program to physical squin with noise models inserted.
+# This is done using the `MoveToSquin` transformer inside the `bloqade.lanes.transform` module.
+# this transformation requires the physical architecture spec, a logical initialization function
+# to prepare logical qubit from the physical qubits, and a noise model to insert noise channels during
+# the compilation.
+
+# %%
+
+from bloqade.lanes.arch.gemini.logical import steane7_initialize  # noqa: E402
+from bloqade.lanes.noise_model import generate_simple_noise_model  # noqa: E402
+from bloqade.lanes.transform import MoveToSquin  # noqa: E402
+
+noise_model = generate_simple_noise_model()
+example_kernel = MoveToSquin(
+    arch_spec=physical_arch,
+    logical_initialization=steane7_initialize,
+    noise_model=noise_model,
+).emit(example_kernel)
+
+example_kernel.print()
+
+# %% [markdown]
+# From here we can use the existing tools within bloqade-circuit to simulate the physical squin
+# program with noise, or compile to stim programs for further analysis.

src/bloqade/lanes/_prelude.py
@@ -1,13 +1,13 @@
 from kirin import ir
 from kirin.ir import Method
 from kirin.passes import Default
-from kirin.prelude import basic
+from kirin.prelude import structural
 from typing_extensions import Annotated, Doc
 
 from bloqade.lanes.dialects import move
 
 
-@ir.dialect_group(basic.add(move))
+@ir.dialect_group(structural.add(move))
 def kernel(self):
     def run_pass(
         mt: Annotated[Method, Doc("The method to run pass on.")],

src/bloqade/lanes/arch/gemini/impls.py
@@ -49,7 +49,9 @@ def hypercube_busses(hypercube_dims: int):
     return tuple(word_buses)
 
 
-def generate_arch(hypercube_dims: int = 4, word_size_y: int = 5) -> ArchSpec:
+def generate_arch(
+    hypercube_dims: int = 4, word_size_y: int = 5
+) -> ArchSpec[tuple[float, float]]:
     word_size_x = 2
     num_word_x = 2**hypercube_dims
 

src/bloqade/lanes/logical_mvp.py
@@ -19,6 +19,30 @@ from bloqade.lanes.transform import MoveToSquin
 from bloqade.lanes.upstream import NativeToPlace, PlaceToMove
 
 
+def transversal_rewrites(mt: ir.Method):
+    """Apply transversal rewrite rules to a squin method.
+
+    Args:
+        mt (ir.Method): rewrite the method in place.
+
+    Returns:
+        ir.Method: The rewritten method.
+
+    """
+
+    rewrite.Walk(
+        rewrite.Chain(
+            transversal.RewriteLocations(logical.steane7_transversal_map),
+            transversal.RewriteLogicalInitialize(logical.steane7_transversal_map),
+            transversal.RewriteMoves(logical.steane7_transversal_map),
+            transversal.RewriteGetItem(logical.steane7_transversal_map),
+            transversal.RewriteLogicalToPhysicalConversion(),
+        )
+    ).rewrite(mt.code)
+
+    return mt
+
+
 def compile_squin_to_move(mt: ir.Method, transversal_rewrite: bool = False):
     """Compile a squin kernel to move dialect.
 
@@ -49,15 +73,7 @@ def compile_squin_to_move(mt: ir.Method, transversal_rewrite: bool = False):
         fixed.LogicalMoveScheduler(),
     ).emit(mt)
     if transversal_rewrite:
-        rewrite.Walk(
-            rewrite.Chain(
-                transversal.RewriteLocations(logical.steane7_transversal_map),
-                transversal.RewriteLogicalInitialize(logical.steane7_transversal_map),
-                transversal.RewriteMoves(logical.steane7_transversal_map),
-                transversal.RewriteGetItem(logical.steane7_transversal_map),
-                transversal.RewriteLogicalToPhysicalConversion(),
-            )
-        ).rewrite(mt.code)
+        mt = transversal_rewrites(mt)
 
     passes.TypeInfer(mt.dialects)(mt)
 

src/bloqade/lanes/rewrite/move2squin/gates.py
@@ -19,9 +19,9 @@ from .base import AtomStateRewriter
 @dataclass
 class InsertGates(AtomStateRewriter):
     move_exec_analysis: ForwardFrame[atom.MoveExecution]
-    initialize_kernel: ir.Method[
-        [float, float, float, ilist.IList[qubit.Qubit, Any]], None
-    ]
+    initialize_kernel: (
+        ir.Method[[float, float, float, ilist.IList[qubit.Qubit, Any]], None] | None
+    ) = None
     measurement_index_map: dict[ZoneAddress, dict[LocationAddress, int]] = field(
         init=False, default_factory=dict
     )
@@ -165,6 +165,9 @@ class InsertGates(AtomStateRewriter):
     def rewrite_PhysicalInitialize(
         self, atom_state: atom.AtomState, node: move.PhysicalInitialize
     ) -> rewrite_abc.RewriteResult:
+        if self.initialize_kernel is None:
+            return rewrite_abc.RewriteResult()
+
         nodes_to_insert: list[ir.Statement] = []
         for theta, phi, lam, locations in zip(
             node.thetas, node.phis, node.lams, node.location_addresses

src/bloqade/lanes/transform.py
@@ -4,7 +4,7 @@ from typing import Literal
 from bloqade.rewrite.passes import aggressive_unroll as agg
 from bloqade.squin.rewrite import SquinU3ToClifford
 from kirin import ir, rewrite
-from kirin.dialects import ilist
+from kirin.dialects import ilist, scf
 from kirin.passes import TypeInfer
 
 from bloqade import qubit, squin
@@ -21,14 +21,15 @@ from bloqade.lanes.rewrite.move2squin import (
 @dataclass
 class MoveToSquin:
     arch_spec: layout.ArchSpec
-    logical_initialization: ir.Method[
-        [float, float, float, ilist.IList[qubit.Qubit, Literal[7]]], None
-    ]
+    logical_initialization: (
+        ir.Method[[float, float, float, ilist.IList[qubit.Qubit, Literal[7]]], None]
+        | None
+    ) = None
     noise_model: NoiseModelABC | None = None
     aggressive_unroll: bool = False
 
     def emit(self, main: ir.Method, no_raise: bool = True) -> ir.Method:
-        main = main.similar(main.dialects.union(squin.kernel))
+        main = main.similar(main.dialects.union(squin.kernel.discard(scf.lowering)))
 
         vqpu = atom.AtomInterpreter(main.dialects, arch_spec=self.arch_spec)
         run_method = vqpu.run_no_raise if no_raise else vqpu.run

commit fae10b9fa2fee221702bb989fe05ccdaa48869d2

Fixing bug introduced in pr #100 (#101)
* fixing bug introducted in old pr and fixing issue with recursion when comparing ssa values

* renaming variable to be more clear

src/bloqade/lanes/rewrite/move2squin/noise.py
@@ -109,33 +109,35 @@ class InsertNoise(AtomStateRewriter):
             return rewrite_abc.RewriteResult()
 
         if not isinstance(
-            atom_state := self.atom_state_map.get(trait.get_state_result(node)),
+            state_after := self.atom_state_map.get(trait.get_state_result(node)),
             atom.AtomState,
         ):
             return rewrite_abc.RewriteResult()
 
-        return self.rewriter(node, atom_state)
+        return self.rewriter(node, state_after)
 
     @singledispatchmethod
-    def rewriter(self, node: ir.Statement, atom_state: atom.AtomState):
+    def rewriter(self, node: ir.Statement, state_after: atom.AtomState):
         return rewrite_abc.RewriteResult()
 
     @rewriter.register(move.Move)
     def _(
-        self, node: move.Move, atom_state: atom.AtomState
+        self, node: move.Move, state_after: atom.AtomState
     ) -> rewrite_abc.RewriteResult:
         if len(node.lanes) == 0:
             return rewrite_abc.RewriteResult()
 
         move_noise_methods = tuple(map(self.noise_model.get_lane_noise, node.lanes))
         qubit_ssas = self.get_qubit_ssa_from_locations(
-            atom_state,
-            tuple(self.arch_spec.get_endpoints(lane)[0] for lane in node.lanes),
+            state_after,
+            tuple(self.arch_spec.get_endpoints(lane)[1] for lane in node.lanes),
         )
-        qubit_ssas = list(filter(None, qubit_ssas))
+        qubit_ssas = tuple(filter(None, qubit_ssas))
+
+        qubit_ssa_set = set(qubit_ssas)
 
         def is_stationary(qubit_ssa: ir.SSAValue) -> bool:
-            return qubit_ssa not in qubit_ssas
+            return qubit_ssa not in qubit_ssa_set
 
         stationary_qubits = tuple(
             filter(is_stationary, self.physical_ssa_values.values())