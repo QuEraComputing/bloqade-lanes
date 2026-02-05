2b9b4382173a66af72ac6d240931200c14e5b540
Phillip Weinberg <weinbe58@gmail.com>
2026-02-05 14:54:28 -0500

    fix linter

--- src/bloqade/lanes/visualize.py
+++ src/bloqade/lanes/visualize.py
@@ -50,10 +50,10 @@ class StateArtist:
 
         for lane in state.data.prev_lanes.values():
             start, end = arch_spec.get_endpoints(lane)
-            (x_start, y_start) = arch_spec.words[start.word_id].site_position(
+            x_start, y_start = arch_spec.words[start.word_id].site_position(
                 start.site_id
             )
-            (x_end, y_end) = arch_spec.words[end.word_id].site_position(end.site_id)
+            x_end, y_end = arch_spec.words[end.word_id].site_position(end.site_id)
             ax.quiver(
                 [x_start],
                 [y_start],
