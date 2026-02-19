791ce9b4567e42d1996f0d38158ec4374292531b
Phillip Weinberg <weinbe58@gmail.com>
2026-02-19 16:20:21 -0500

    adding tests for post_processing

--- src/bloqade/lanes/analysis/atom/__init__.py
+++ src/bloqade/lanes/analysis/atom/__init__.py
@@ -7,10 +7,12 @@ from .atom_state_data import AtomStateData as AtomStateData
 from .lattice import (
     AtomState as AtomState,
     Bottom as Bottom,
+    DetectorResult as DetectorResult,
     IListResult as IListResult,
     MeasureFuture as MeasureFuture,
     MeasureResult as MeasureResult,
     MoveExecution as MoveExecution,
+    ObservableResult as ObservableResult,
     Unknown as Unknown,
     Value as Value,
 )
