fixing pauli frame issue with state prep and update demo to use KL divergence to compare results

demo/simulator_device_demo.py
@@ -1,4 +1,5 @@
 import math
+from collections import Counter
 from typing import Any
 
 import numpy as np
@@ -9,6 +10,26 @@ from kirin.dialects import ilist
 
 from bloqade import qubit, squin, types
 from bloqade.lanes.device import GeminiLogicalSimulator
+
+
+# helper functions to analyze statical distribution of logical measurements
+def get_hist(obs_array: np.ndarray):
+    return Counter(map(lambda x: tuple(map(int, x)), obs_array[:]))
+
+
+def kl_divergence(p_hist: Counter, q_hist: Counter) -> float:
+    """Compute the KL divergence D_KL(P || Q) between two histograms."""
+    total_p = sum(p_hist.values())
+    total_q = sum(q_hist.values())
+    divergence = 0.0
+    for key in p_hist:
+        p_prob = p_hist[key] / total_p
+        q_prob = q_hist.get(key, 0) / total_q
+        if q_prob > 0:
+            divergence += p_prob * math.log(p_prob / q_prob)
+        else:
+            divergence += p_prob * math.log(p_prob / (1e-10))  # Avoid log(0)
+    return divergence
 
 
 @logical.kernel(aggressive_unroll=True, verify=False)
@@ -68,19 +89,32 @@ def main() -> tuple[list[Detector], list[Observable]]:
 
 task = GeminiLogicalSimulator().task(main)
 
-result = task.run(10, with_noise=True)
-result_wo_noise = task.run(10, with_noise=False)
-return_values = result.return_values
+result = task.run(100000)
+result_wo_noise = task.run(100000, with_noise=False)
 detectors = np.asarray(result.detectors)
 observables = np.asarray(result.observables)
+observables_without_noise = np.asarray(result_wo_noise.observables)
 
-flips = BpLsdDecoder(result.detector_error_model).decode(detectors)
+flips = BpLsdDecoder(task.detector_error_model).decode(detectors)
+
+
+observables_hist = get_hist(observables)
+observables_decoded_hist = get_hist(observables ^ flips)
+observables_wo_noise_hist = get_hist(observables_without_noise)
+
+post_selection = np.all(detectors == 0, axis=1)
+observables_postselected = observables[post_selection, :]
+observables_postselected_hist = get_hist(observables_postselected)
 
 print(
-    "average error without decoding:",
-    np.mean(observables ^ result_wo_noise.observables, axis=0),
+    "KL divergence between noisy and raw observables:",
+    kl_divergence(observables_wo_noise_hist, observables_hist),
 )
 print(
-    "average error with decoding:",
-    np.mean((observables ^ flips) ^ result_wo_noise.observables, axis=0),
+    "KL divergence between noisy and decoded observables:",
+    kl_divergence(observables_wo_noise_hist, observables_decoded_hist),
+)
+print(
+    "KL divergence between noisy and post-selected observables:",
+    kl_divergence(observables_wo_noise_hist, observables_postselected_hist),
 )