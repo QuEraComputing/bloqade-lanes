cd120af4a88f152603774eab26563011de5f3aaa
David Plankensteiner <dplankensteiner@quera.com>
2026-05-27 11:00:53 +0200

    Fix test for bloqade-core v0.6.3

--- python/tests/gemini/device/test_task.py
+++ python/tests/gemini/device/test_task.py
@@ -150,7 +150,7 @@ def test_single_kernel_task_create_task_definition_int_num_shots():
     task = GeminiSingleKernelTask(kernel=bell, num_shots=10)
     task_def = task.create_task_definition()
 
-    assert task_def.program_language == "squin"
+    assert task_def.program_language.startswith("squin.v")
     assert len(task_def.programs) == 1
     assert len(task_def.subtasks) == 1
     subtask = task_def.subtasks[0]
