870e9325b76f3bcdc3576e3397ccc79e563def60
weinbe58 <weinbe58@users.noreply.github.com>
2026-06-08 14:53:00 +0000

    deploy: 96b43845770608ccf84d80fb9eef5b6b0ae19ca9

--- dev/api/bloqade_bytecode/policy/trace/sidebar-items.js
+++ dev/api/bloqade_bytecode/policy/trace/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["loc_from_triple","run_trace_policy","trace_move","trace_target"],"struct":["HumanMoveTraceObserver","HumanTargetTraceObserver"]};
+window.SIDEBAR_ITEMS = {"fn":["run_trace_policy","trace_move","trace_target"],"struct":["HumanMoveTraceObserver","HumanTargetTraceObserver"]};
