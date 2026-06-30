07b4adf2191795ddc4f6e58a81dcd53e1d470dfe
Jason Han <jhan@quera.com>
2026-06-30 11:50:52 -0400

    feat: added physical simulator demo

--- demo/physical_arch_customization.py
+++ demo/physical_arch_customization.py
@@ -30,13 +30,16 @@
 
 # %% [markdown]
 # Before we get started, it's useful to define how we address our architecture. We have three "levels" to addressing atoms in our architecture: zones, words, and sites. A concrete depiction of the architecture for Gemini physical is shown below:
-
+#
 # Import utilities to define the SQuIN kernel dialect that we will be writing our programs in.
 
 # %%
 from typing import Any, Literal, TypeVar
 
 import matplotlib.pyplot as plt
+
+# For postprocessing
+import numpy as np
 from bloqade.types import Qubit
 from kirin.dialects import ilist
 from kirin.dialects.ilist import IList
@@ -425,7 +428,12 @@ physical_msd_task = simulator.task(
 # %%
 physical_msd_task.visualize()
 
-# %%
-physical_msd_task.physical_move_kernel.print()
+# %% [markdown]
+# ## Running Tasks using Physical Simulator
+# Similar to the simulator task for the logical simulator, we can also run the tasks for the physical simulator using "task.run()".
 
 # %%
+physical_msd_task_res = physical_msd_task.run(shots=1000)
+
+# %%
+print(np.array(physical_msd_task_res.measurements).shape)
