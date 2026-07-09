b7a97e803eddc0bbfc61777fbe0b3976f8b3f47a
weinbe58 <weinbe58@users.noreply.github.com>
2026-07-09 17:50:54 +0000

    deploy: 091f6364053e5fed4eea5c62932d4c9ed590e9e7

--- dev/api/trait.impl/bloqade_lanes_search/dsl/move_policy_dsl/observer/trait.MoveKernelObserver.js
+++ dev/api/trait.impl/bloqade_lanes_search/dsl/move_policy_dsl/observer/trait.MoveKernelObserver.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["bloqade_bytecode",[["impl&lt;W: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.1/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a>&gt; MoveKernelObserver for <a class=\"struct\" href=\"bloqade_bytecode/policy/trace/struct.HumanMoveTraceObserver.html\" title=\"struct bloqade_bytecode::policy::trace::HumanMoveTraceObserver\">HumanMoveTraceObserver</a>&lt;W&gt;",0]]]]);
+    const implementors = Object.fromEntries([["bloqade_bytecode",[["impl&lt;W: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.97.0/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a>&gt; MoveKernelObserver for <a class=\"struct\" href=\"bloqade_bytecode/policy/trace/struct.HumanMoveTraceObserver.html\" title=\"struct bloqade_bytecode::policy::trace::HumanMoveTraceObserver\">HumanMoveTraceObserver</a>&lt;W&gt;",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- dev/api/trait.impl/bloqade_lanes_search/dsl/target_generator_dsl/observer/trait.TargetKernelObserver.js
+++ dev/api/trait.impl/bloqade_lanes_search/dsl/target_generator_dsl/observer/trait.TargetKernelObserver.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["bloqade_bytecode",[["impl&lt;W: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.1/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a>&gt; TargetKernelObserver for <a class=\"struct\" href=\"bloqade_bytecode/policy/trace/struct.HumanTargetTraceObserver.html\" title=\"struct bloqade_bytecode::policy::trace::HumanTargetTraceObserver\">HumanTargetTraceObserver</a>&lt;W&gt;",0]]]]);
+    const implementors = Object.fromEntries([["bloqade_bytecode",[["impl&lt;W: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.97.0/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a>&gt; TargetKernelObserver for <a class=\"struct\" href=\"bloqade_bytecode/policy/trace/struct.HumanTargetTraceObserver.html\" title=\"struct bloqade_bytecode::policy::trace::HumanTargetTraceObserver\">HumanTargetTraceObserver</a>&lt;W&gt;",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
