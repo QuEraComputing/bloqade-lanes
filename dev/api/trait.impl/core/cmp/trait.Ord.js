c3dd27c909bcbe78f2e8016bc9ba8bda42fc91a2
weinbe58 <weinbe58@users.noreply.github.com>
2026-07-24 14:15:28 +0000

    deploy: 94a72afb02ae82331ebcd839c800c4a40108a8eb

---
+++ dev/api/trait.impl/core/cmp/trait.Ord.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["bloqade_lanes_bytecode_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.97.1/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"bloqade_lanes_bytecode_core/arch/addr/enum.Direction.html\" title=\"enum bloqade_lanes_bytecode_core::arch::addr::Direction\">Direction</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.97.1/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"bloqade_lanes_bytecode_core/arch/addr/enum.MoveType.html\" title=\"enum bloqade_lanes_bytecode_core::arch::addr::MoveType\">MoveType</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[638]}
