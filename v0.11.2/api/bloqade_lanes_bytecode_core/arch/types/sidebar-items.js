99afd319a95332fce4a9cf854bb2fd8c860176c0
weinbe58 <weinbe58@users.noreply.github.com>
2026-08-04 19:07:50 +0000

    deploy: 73d639975b3954e9b2a93783246523c86be6de4a

---
+++ v0.11.2/api/bloqade_lanes_bytecode_core/arch/types/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["ArchSpec","Bus","Grid","Mode","TransportPath","Word","Zone"]};
---
+++ v0.11.2/api/bloqade_lanes_bytecode_core/isa/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"mod":["def","parse_helpers","program","text","validate"]};
