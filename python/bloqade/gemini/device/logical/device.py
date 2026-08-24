13ae0b8c51a2c20b4cae965974a38550860d83ce
Jason Han <jhan@quera.com>
2026-08-24 16:14:59 -0400

    feat: added fields to the device to support core impl

--- python/bloqade/gemini/device/logical/device.py
+++ python/bloqade/gemini/device/logical/device.py
@@ -1,14 +1,31 @@
 from dataclasses import dataclass, field
+from importlib.metadata import version
 
+from bloqade.analysis.validation.simple_nocloning import FlatKernelNoCloningValidation
 from bloqade.core.device import Device
 from bloqade.core.device.task import (
     KernelBatchTask,
     ParameterScanTask,
     SingleKernelTask,
 )
+from kirin import ir
+from kirin.validation import ValidationSuite
 
+from bloqade.gemini.logical.group import kernel as logical_kernel
+
+from ...common.validation.duplicate_address import (
+    DuplicateAddressValidation,
+)
+from ...logical.validation.clifford.analysis import (
+    GeminiLogicalValidation,
+)
+from ...logical.validation.measurement.analysis import (
+    GeminiTerminalMeasurementValidation,
+)
 from .future import GeminiLogicalFuture
 from .task import GeminiKernelBatchTask, GeminiParameterScanTask, GeminiSingleKernelTask
+
+_bloqade_version = version("bloqade-circuit")
 
 
 @dataclass(kw_only=True)
@@ -18,6 +35,20 @@ class GeminiLogicalDevice(Device[GeminiLogicalFuture]):
     Wires the Gemini-specific task and future classes into the generic
     `Device` factory and defaults `context_name` to "gemini-logical".
     """
+
+    program_language: str = "squin"
+    language_version: str = "0.1.0"
+    validation_suite: ValidationSuite | None = field(
+        default_factory=lambda: ValidationSuite(
+            [
+                GeminiLogicalValidation,
+                GeminiTerminalMeasurementValidation,
+                FlatKernelNoCloningValidation,
+                DuplicateAddressValidation,
+            ]
+        )
+    )
+    dialect_group: ir.DialectGroup = logical_kernel
 
     future_cls: type[GeminiLogicalFuture] = GeminiLogicalFuture
     single_kernel_task_cls: type[SingleKernelTask[GeminiLogicalFuture]] = field(
