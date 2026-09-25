609b525b98ec65c71785c65e94004ea37a0ddd0b
Jason Han <jhan@quera.com>
2026-09-24 23:56:55 -0400

    refactor: re-exported squin gates in logical module

--- python/bloqade/gemini/logical/__init__.py
+++ python/bloqade/gemini/logical/__init__.py
@@ -1,3 +1,31 @@
+from bloqade.qubit import qalloc as qalloc
+from bloqade.squin.stdlib.simple import (
+    cnot as cnot,
+    cx as cx,
+    cy as cy,
+    cz as cz,
+    h as h,
+    rx as rx,
+    ry as ry,
+    rz as rz,
+    s as s,
+    s_adj as s_adj,
+    shift as shift,
+    sqrt_x as sqrt_x,
+    sqrt_x_adj as sqrt_x_adj,
+    sqrt_y as sqrt_y,
+    sqrt_y_adj as sqrt_y_adj,
+    sqrt_z as sqrt_z,
+    sqrt_z_adj as sqrt_z_adj,
+    swap as swap,
+    t as t,
+    t_adj as t_adj,
+    u3 as u3,
+    x as x,
+    y as y,
+    z as z,
+)
+
 from bloqade.gemini.common.dialects.arrange import move_to as move_to
 from bloqade.lanes.dialects.arch import loc as loc
 
